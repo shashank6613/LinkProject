35784e62b1c289ba78167560e4e54e32d41e0a3c
shashank6613 <133555467+shashank6613@users.noreply.github.com>
2025-03-04 15:38:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -106,7 +106,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
