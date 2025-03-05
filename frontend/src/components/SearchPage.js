3f4550369fc6e2b6c1c85d0b3973db47ccaaf131
shashank6613 <133555467+shashank6613@users.noreply.github.com>
2025-03-05 23:47:26 +0530

    Update SearchPage.js

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -21,7 +21,7 @@ const SearchPage = () => {
 
     try {
       const queryParams = new URLSearchParams(searchParams).toString();
-      const response = await fetch(`http://backend:5000/api/search?${queryParams}`);
+      const response = await fetch(`http://backend-service:5000/api/search?${queryParams}`);
       const data = await response.json();
       setSearchResults(data);
 
