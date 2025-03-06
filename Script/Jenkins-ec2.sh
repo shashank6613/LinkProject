c48be30368659bbc200ebeb8905c7fc0daa5f92e
shashank6613 <133555467+shashank6613@users.noreply.github.com>
2025-03-06 11:26:16 +0530

    Update Jenkins-ec2.sh

--- Script/Jenkins-ec2.sh
+++ Script/Jenkins-ec2.sh
@@ -6,7 +6,7 @@ sudo apt install curl -y
 sudo apt update
 sudo apt install python3 -y
 sudo apt update
-wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io-2023.key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
