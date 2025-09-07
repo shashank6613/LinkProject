89aea347e814dc4169226546835618e8c00a74ac
shashank6613 <133555467+shashank6613@users.noreply.github.com>
2025-09-07 12:04:28 +0530

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,77 +1,108 @@
+# -------------------------
+# AWS Region
+# -------------------------
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy into"
   type        = string
   default     = "us-west-2"
 }
 
+# -------------------------
+# VPC and Subnets
+# -------------------------
 variable "vpc_cidr" {
-  description = "VPC CIDR"
+  description = "VPC CIDR block"
   type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDRs (2)"
+  description = "List of public subnet CIDRs"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of private subnet CIDRs (2)"
+  description = "List of private subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zones" {
-  description = "Availability zones to use (length should match subnet counts)"
+  description = "Availability zones to spread subnets"
   type        = list(string)
   default     = ["us-west-2a", "us-west-2b"]
 }
 
+# -------------------------
+# EKS Config
+# -------------------------
 variable "eks_cluster_name" {
-  description = "EKS cluster name"
+  description = "EKS Cluster Name"
   type        = string
-  default     = "link-clus"
-}
-
-variable "node_group_desired" { default = 1 }
-variable "node_group_min"     { default = 1 }
-variable "node_group_max"     { default = 2 }
-
-variable "node_instance_type" {
-  default = "t3.medium"
-}
-
-variable "master_ec2_instance_type" {
-  default = "t2.medium"
-}
-
-variable "ssh_key_name" {
-  description = "Existing EC2 Key pair name to attach to EC2 for SSH (must exist in region)"
-  type        = string
-  default     = "linked"
-}
-
-variable "master_ec2_ami" {
-  description = "AMI for the master EC2 (replace with region-specific Ubuntu AMI)"
-  type        = string
-  default     = "ami-REPLACE_ME"
+  default     = "linkproj-eks"
 }
 
 variable "eks_version" {
-  description = "EKS Kubernetes version"
+  description = "Kubernetes version for EKS"
   type        = string
-  default     = "1.27"
+  default     = "1.29"
 }
 
-variable "db_name"    { default = "primarydb" }
-variable "db_user"    { default = "pgadmin" }
-variable "db_password" {
-  description = "RDS master password"
+variable "node_instance_type" {
+  description = "Instance type for EKS nodes"
   type        = string
-  default     = "ChangeMe123!"
+  default     = "t3.medium"
+}
+
+variable "node_group_desired" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_group_min" {
+  description = "Minimum worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "node_group_max" {
+  description = "Maximum worker nodes"
+  type        = number
+  default     = 3
+}
+
+# -------------------------
+# RDS Config
+# -------------------------
+variable "primary_rds_identifier" {
+  description = "Primary RDS instance identifier"
+  type        = string
+  default     = "myprimarypgdb"
+}
+
+variable "replica_rds_identifier" {
+  description = "Replica RDS instance identifier"
+  type        = string
+  default     = "myreplicapgdb"
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "primarydb"
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+  default     = "shank"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  default     = "admin12345"
   sensitive   = true
 }
-
-variable "primary_rds_identifier" { default = "MyPrimaryPGDB" }
-variable "replica_rds_identifier" { default = "MyReplicaPGDB" }
