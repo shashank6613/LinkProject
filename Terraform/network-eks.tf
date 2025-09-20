3b1a40faf7b75f9f0c79ea99bfe2aecb744ef268
shank <shank@gmail.com>
2025-09-20 13:38:37 +0000

    edited thumbprint in terraform

--- Terraform/network-eks.tf
+++ Terraform/network-eks.tf
@@ -335,15 +335,10 @@ resource "aws_eks_node_group" "managed_nodes" {
 # IRSA for Backend Pods (NEW)
 # ------------------------
 
-# OIDC provider for EKS (needed once per cluster)
-data "aws_eks_cluster" "eks" {
-  name = aws_eks_cluster.cluster.name
-}
-
 resource "aws_iam_openid_connect_provider" "eks" {
   url             = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd2a3a5"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
 }
 
 # Trust policy for backend service account
